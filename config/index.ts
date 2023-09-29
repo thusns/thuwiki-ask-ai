00742a67bf2819e00749b3a05ea1537a51862439
Cactus <erbanku@gmail.com>
2023-09-29 22:27:38 +0800

    add and modify some configurations

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = '1aa9dad2-7fc8-4157-8220-cb6e7a36813a'
+export const API_KEY = 'app-0IuiYdookAye73meeSxlOiVU'
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+    title: 'app-0IuiYdookAye73meeSxlOiVU',
+    description: '',
+    copyright: '© THU Wiki, 2023',
+    privacy_policy: '',
+    default_language: 'en',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'Act as a THU Wiki AI assistant, answer user queries about Tsinghua University according to Wiki content precisely and carefully, be friendly, and use some emojis if needed. Now is the year 2023.'
 
 export const API_PREFIX = '/api'
 
